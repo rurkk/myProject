f329f95950eeee942ba9d0565b9ec0a1c7d263dc
rurkk <367414@niuitmo.ru>
2022-12-20 23:46:31 +0300

    another

--- 1.py
+++ 1.py
@@ -1,32 +1,49 @@
-import main
 import unittest
 
 class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         '''Test case function for addition'''
-        self.calc = main.Calculator()
+        self.calc = Calculator()
         result = self.calc.add(4, 7)
         expected = 11
         self.assertEqual(result, expected)
 
     def test_sub(self):
         '''Test case function for subtraction'''
-        self.calc = main.Calculator()
+        self.calc = Calculator()
         result = self.calc.sub(10, 5)
         expected = 5
         self.assertEqual(result, expected)
 
     def test_mul(self):
         '''Test case function for multiplication'''
-        self.calc = main.Calculator()
+        self.calc = Calculator()
         result = self.calc.mul(3, 7)
         expected = 21
         self.assertEqual(result, expected)
 
     def test_div(self):
         '''Test case function for division'''
-        self.calc = main.Calculator()
+        self.calc = Calculator()
         result = self.calc.div(10, 2)
         expected = 5
         self.assertEqual(result, expected)
+
+
+class Calculator:
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        if b != 0:
+            return a / b

3ad6e99f8096bd56d0b3652a39570f73547b0b60
rurkk <367414@niuitmo.ru>
2022-12-20 23:30:07 +0300

    python project

---
+++ main.py
@@ -0,0 +1,18 @@
+import unittest
+
+class Calculator:
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        if b != 0:
+            return a / b
